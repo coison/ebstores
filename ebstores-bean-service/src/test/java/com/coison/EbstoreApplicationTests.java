68aa0612cffd74c68b726ccd30423eecf96dbab6
maiyar <1308765559@qq.com>
2017-04-28 18:06:56 +0800

    spring boot  + mybatis generator 自动生成dao,model,以及mapper文件.

---
+++ ebstores-bean-service/src/test/java/com/coison/EbstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coison;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EbstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
