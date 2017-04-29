ab8dbdb4ab33574889e7d020223ede14208e18d3
maiyar <1308765559@qq.com>
2017-04-30 01:31:23 +0800

    spring boot admin

---
+++ ebstores-members-service/src/main/java/com/coison/TestController.java
@@ -0,0 +1,25 @@
+package com.coison;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * Created by Administrator on 2017/4/28.
+ */
+@RestController
+@EnableAutoConfiguration
+public class TestController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/hello/{myName}")
+    String index(@PathVariable String myName) {
+        return "Hello "+myName+"!!!";
+    }
+}
---
+++ eureka-service-web/src/test/java/com/coison/eureka/EurekaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coison.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
