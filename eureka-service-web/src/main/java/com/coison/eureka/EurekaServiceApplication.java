9b6d30afb3a83772cb2ee5f1e67a6d205392ecab
maiyar <1308765559@qq.com>
2017-04-30 21:55:44 +0800

    注册中心

--- eureka-service-web/src/main/java/com/coison/eureka/EurekaServiceApplication.java
+++ eureka-service-web/src/main/java/com/coison/eureka/EurekaServiceApplication.java
@@ -7,6 +7,7 @@ import de.codecentric.boot.admin.notify.RemindingNotifier;
 import de.codecentric.boot.admin.notify.filter.FilteringNotifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
