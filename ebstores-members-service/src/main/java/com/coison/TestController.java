79f141adfc21b6cff484fea481990266a170b2c5
maiyar <1308765559@qq.com>
2017-04-30 21:54:41 +0800

    注册服务

--- ebstores-members-service/src/main/java/com/coison/TestController.java
+++ ebstores-members-service/src/main/java/com/coison/TestController.java
@@ -1,8 +1,10 @@
 package com.coison;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,5 +23,13 @@ public class TestController {
     @RequestMapping("/hello/{myName}")
     String index(@PathVariable String myName) {
         return "Hello "+myName+"!!!";
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" + port;
     }
 }
