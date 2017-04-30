0e02bba56a091cedeb438e2dd3bcb10631076273
maiyar <1308765559@qq.com>
2017-04-30 17:43:13 +0800

    用户服务提供者.

--- ebstores-members-service/src/main/java/com/coison/EbstoresMembersServiceApplication.java
+++ ebstores-members-service/src/main/java/com/coison/EbstoresMembersServiceApplication.java
@@ -2,9 +2,7 @@ package com.coison;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient  //激活eureka中的DiscoveryClient实现
 @SpringBootApplication
 public class EbstoresMembersServiceApplication {
 
