77d244d665a9da3d438cedebcdb3e116272b9b39
maiyar <1308765559@qq.com>
2017-05-02 23:53:34 +0800

    spring cloud config 云配置中心

--- config-server/src/main/java/com/coison/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/coison/config/ConfigServerApplication.java
@@ -3,6 +3,8 @@ package com.coison.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /***
  * EnableConfigServer 分析
@@ -12,7 +14,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class ConfigServerApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "云配置中心";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
